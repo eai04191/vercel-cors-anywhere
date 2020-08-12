14b9c3906a0037dcc13b3f7fec5b8211e67ded8b
Irie Aoi <eai@mizle.net>
2020-08-12 17:40:11 +0900

    add cookieJar

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,27 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+import { promisify } from "util";
 import got from "got";
+import { CookieJar } from "tough-cookie";
 
 export default async function (req: NowRequest, res: NowResponse) {
     const { url = null } = req.query;
     if (url === null) {
-        res.status(400).send(`Missing url parameter.`);
+        res.status(400).json({ error: `Missing url parameter.` });
         return;
     }
     if (typeof url !== "string") {
-        res.status(400).send(`url parameter must be string.`);
+        res.status(400).json({ error: `url parameter must be string.` });
         return;
     }
 
-    const response = await got(url).text();
+    try {
+        const cookieJar = new CookieJar();
+        const response = await got(url, { cookieJar });
 
-    res.send(response);
+        res.send(response.body);
+        return;
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+        return;
+    }
 }
